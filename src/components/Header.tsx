ab3a682e76b66ee610bce63e3b362b04528ee6f4
Emilien Vaschalde <emilienvaschalde@yahoo.fr>
2022-12-07 08:35:50 +0100

    Header & Guide on HOME page [responsive tel]

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,37 @@
+import { useEffect, useState } from "react";
 
 const Header = () => {
+    const [displayHeader, setDisplayHeader] = useState(false)
 
+    useEffect(() => {
+        console.log(window.innerWidth)
+        if(window.innerWidth > 650) {
+            // header always ON on desktop
+            setDisplayHeader(true)
+        } else {
+            setDisplayHeader(false)
+        }
+    }, [])
 
     return (
+        <div>
+            { !displayHeader && <i onClick={() => setDisplayHeader(true)} className="fa-solid fa-bars headerTelIcon"></i> }
+            { displayHeader && <HeaderBloc setDisplayHeader={setDisplayHeader} /> }
+        </div>
+    );
+};
+
+export default Header;
+
+
+interface PropsHeaderBloc {
+    setDisplayHeader: (value: boolean) => void
+}
+
+const HeaderBloc = ({ setDisplayHeader }: PropsHeaderBloc) => {
+    return (
         <div className="Header">
+            <div className="cross" onClick={() => setDisplayHeader(false)}>&times;</div>
             <nav>
                 <div className="blocLien">
                     <a href="">Guitare électriques</a>
@@ -18,9 +46,6 @@ const Header = () => {
                     <a href="">Accessoires</a>
                 </div>
             </nav>
-
         </div>
-    );
-};
-
-export default Header;
+    )
+}
