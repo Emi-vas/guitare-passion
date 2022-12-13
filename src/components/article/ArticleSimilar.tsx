c7c7120da61a402991f0e9ed17a4ddc66a985960
Emilien Vaschalde <emi@macbook-air-de-emilien.home>
2022-12-13 10:39:03 +0100

    ArticleSimilar logic

--- src/components/article/ArticleSimilar.tsx
+++ src/components/article/ArticleSimilar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Guitar } from '../../assets/types';
 import ArticleCard from '../ArticleCard';
 
@@ -10,13 +10,48 @@ interface Props {
 const ArticleSimilar = ({ data, articleRef }: Props) => {
     const [dataSimilar, setDataSimilar] = useState<undefined | Guitar[]>()
 
+    useEffect(() => {
+        let tempsSimilar: any = []
+        let styleMain = articleRef.style[0]
 
+        //Seek for articles whith same style of product
+        if(data && articleRef) {
+            data.map(article => {
+                article.style.map(style => {
+                    if (style == styleMain){
+                        //remove the articleRef
+                        if(article.id != articleRef.id) {
+                            tempsSimilar = [...tempsSimilar, article]
+                        }
+                    }
+                })
+            })
+        }
+
+        let tempSimilarWithPrice: any = []
+        
+        //Filtred with price -500 and +500 of article ref
+        tempsSimilar.map((article: any) => {
+            const dif = article.price - articleRef.price
+            console.log(dif)
+            if(dif <= 500 && dif >= -500) {
+                tempSimilarWithPrice = [...tempSimilarWithPrice, article]
+            }
+        })
+
+        //apply the filter if there are enough articles
+        if(tempSimilarWithPrice.length >= 3) {
+            tempsSimilar = tempSimilarWithPrice
+        }
+
+        setDataSimilar(tempsSimilar.slice(0, 3))
+    },[data, articleRef])
 
     return (
         <div className='othersSection'>
-            <h2>Autres Guitares</h2>
+            <h2>Articles similaires</h2>
             {
-                data.slice(0, 5).map((article) => (
+                dataSimilar && dataSimilar.map((article) => (
                     <ArticleCard article={article} key={article.id}/>
                 ))
             }
