d08f0458a8c994f5c908faa59ddfae1dee1d1728
Emilien Vaschalde <emi@macbook-air-de-emilien.home>
2022-12-30 12:56:32 +0100

    test total cart

--- src/pages/Cart/__test__/Cart.test.tsx
+++ src/pages/Cart/__test__/Cart.test.tsx
@@ -7,24 +7,50 @@ import { addToCart } from "../../../redux/shoppingCart/shoppingCart.actions";
 import { createStore } from "redux";
 import rootReducer from "../../../redux/rootReducer";
 import userEvent from "@testing-library/user-event";
+import { Guitar } from "../../../assets/types";
+import { useEffect } from "react";
+
+const anArticle2 = {
+    id: 2,
+    name: "Harley Benton SC-450Plus LD Vintage Series",
+    price: 198,
+    rate: 4.5,
+    img: "4.png",
+    cara: {
+        polyvalence: 5,
+        maneuverability: 4,
+        sound: 4
+    },
+    style: ["rock", "hard-rock", "blues", "pop", "jazz"],
+    desc: "Harley Benton est une marque créée pour et distribuée par le détaillant allemand Thomann. Créée en 19971, elle propose des guitares, basses, banjos, mandolines, microphones, amplificateurs, pédales d'effet, des cordes, des médiators, des pièces détachées, etc., souvent à des prix peu élevés (qui s'expliquent en partie par une fabrication uniquement asiatique et donc très bon marché). Le succès de cette marque s'inscrit dans une tendance de fond d'amélioration de la qualité des instruments peu chers.",
+    video: "https://www.youtube.com/embed/nGCVtbrzzio"
+}
 
 interface Props {
-    withArticle ?: boolean
+    withArticle : Guitar[] | false
 }
 
 const CartMock = ({withArticle}: Props) => {
     return(
         <BrowserRouter>
             <Provider store={createStore(rootReducer)}>
-                { withArticle ? <CartWithArticle /> : <Cart /> }
+                { withArticle ? <CartWithArticle articleList={withArticle} /> : <Cart /> }
             </Provider>
         </BrowserRouter>
     )
 }
 
-const CartWithArticle = () => {
+interface CartWithArticleProps {
+    articleList: Guitar[]
+}
+
+const CartWithArticle = ({articleList}: CartWithArticleProps) => {
     const dispatch = useDispatch()
-    dispatch(addToCart(anArticle))
+
+    useEffect(() => {
+        articleList.map((a: any) => dispatch(addToCart(a)))
+    },[])
+
     return(<Cart />)
 }
 
@@ -36,31 +62,38 @@ describe("Cart page testing", ()=> {
         expect(text).toBeInTheDocument()
     })
     test("not empty cart", () => {
-        render(<CartMock withArticle={true} />)
+        render(<CartMock withArticle={[anArticle]} />)
         const text = screen.queryByText(/votre panier est vide/i)
         expect(text).not.toBeInTheDocument()
     })
     test("display title and price of article in cart", () => {
-        render(<CartMock withArticle={true} />)
+        render(<CartMock withArticle={[anArticle]} />)
         const title = screen.getByText(/Harley Benton SC-1000 SBK Progressive Line/i)
-        const price = screen.getByText(/198/i)
+        const price = screen.getAllByText(/198/i)
         expect(title).toBeInTheDocument()
-        expect(price).toBeInTheDocument()
+        expect(price.length).toBe(2)
     })
     test("display sum when there is more then one article & + btn", ()=> {
-        render(<CartMock withArticle={true} />)
+        render(<CartMock withArticle={[anArticle]} />)
         const plusBtn = screen.getByRole('button', {name: "+"})
         userEvent.click(plusBtn)
         const titleUnitaire = screen.getByText(/tarif unitaire/i)
         expect(titleUnitaire).toBeInTheDocument()
-        const sum = screen.getByText(/396/i)
-        expect(sum).toBeInTheDocument()
+        const sum = screen.getAllByText(/396/i)
+        expect(sum.length).toBe(2)
     })
     test('remove an article and clear cart with - btn', () => {
-        render(<CartMock withArticle={true} />)
+        render(<CartMock withArticle={[anArticle]} />)
         const lessBtn = screen.getByRole("button", {name: '-'})
         userEvent.click(lessBtn)
         const text = screen.getByText(/votre panier est vide/i)
         expect(text).toBeInTheDocument()
+    })
+    test("total with multiple articles", () => {
+        render(<CartMock withArticle={[anArticle, anArticle2, anArticle2]} />)
+        const totalTitle = screen.getByRole("heading", {name: /total/i})
+        expect(totalTitle).toBeInTheDocument()
+        const totalValue = screen.getByText("594 €")
+        expect(totalValue).toBeInTheDocument()
     })
 })
--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import TopSection from '../../components/TopSection';
 import { Store, GuitarInCart } from '../../assets/types';
@@ -6,6 +6,13 @@ import CartItem from '../../components/CartItem';
 
 const Cart = () => {
     const cart = useSelector((store: Store ) => store.cart.cart )
+    const [total, setTotal] = useState(0)
+
+    useEffect(() => {
+        let totalTemp = 0
+        cart.forEach((c: GuitarInCart) => totalTemp = totalTemp + (c.price * c.qte))
+        setTotal(totalTemp)
+    },[cart])
 
     return (
         <div className='cart'>
@@ -22,7 +29,7 @@ const Cart = () => {
                 }
                 </div>
 
-                { cart[0] ? <Checkout /> : null }
+                { cart[0] ? <Checkout total={total}/> : null }
             </div>
         </div>
     );
@@ -30,16 +37,19 @@ const Cart = () => {
 
 export default Cart;
 
+interface PropsCheckout {
+    total : number
+}
 
-const Checkout = () => {
-    const cart = useSelector((store: Store ) => store.cart.cart )
+const Checkout = ({total}: PropsCheckout) => {
 
     return(
         <div className='checkout'>
-            <h3>Votre commande</h3>
-            <div>
-
+            <h3>Total</h3>
+            <div className='total'>
+                {total.toLocaleString()} €
             </div>
+            <button className='btn-1'>Procéder au payment</button>
         </div>
     )
 }
